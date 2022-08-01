9909f17ee2aec17d526e4c56f66d6a4f125ee5ae
Peter1907 <peter.nady.1408@gmail.com>
2022-08-01 10:34:08 +0200

    solution 01

--- challenge.js
+++ challenge.js
@@ -1,5 +1,88 @@
 // Start with your code from last challenge.
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(number) {
+    if (!this.head) {
+      this.head = new Node(number);
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new Node(number);
+    this.size += 1;
+  }
+
+  get(index) {
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      if (index === count) {
+        return current.value;
+      }
+      else {
+        current = current.next;
+        count += 1;
+      }
+    }
+    return null;
+  }
+
+  addAt(index, number) {
+    let current = this.head;
+    let count = 0;
+    let previous;
+    if (index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(number, this.head);
+      this.size += 1;
+      return;
+    }
+    while (count < index) {
+      previous = current;
+      current = current.next;
+      count += 1;
+    }
+    let newNode = new Node(number);
+    previous.next = newNode;
+    newNode.next = current;
+    this.size += 1;
+  }
+
+  remove(index) {
+    let current = this.head;
+    let previous;
+    let count = 0;
+    if (number > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.head = current.next;
+    }
+    else {
+      while (count < index) {
+        previous = current;
+        current = current.next;
+        count += 1;
+      }
+      previous.next = current.next;
+    }
+    this.size -= 1;
+  }
+}
 
 const list = new LinkedList()
 
